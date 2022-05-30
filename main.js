8253b33cbd594ca7188b30f999ee3f849135fbc6
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-30 15:09:11 +0100

    made changes in main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const gameSquares = document.querySelectorAll(".jsgame__layout");
 
+let player = "x";
+
 const buttonClick = (event) => {
-  console.log((event.target.innerHTML = "x"));
+  if (player === "x") {
+    event.target.innerHTML = "x";
+    player = "o";
+  } else {
+    event.target.innerHTML = "o";
+    player = "x";
+  }
 };
 
-click.addEventListener("click", buttonClick);
+gameSquares.forEach((square) => {
+  square.addEventListener("click", buttonClick);
+});
+
+// create a variable for the winning combination for the game
+const winningScore = [(1, 5, 9)][(3, 5, 7)][(1, 2, 3)][(4, 5, 6)][(7, 8, 9)][
+  (1, 4, 7)
+][(2, 5, 8)][(3, 6, 9)];
