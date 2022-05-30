b614f02649f3592ae1da2ea65d310e1012018825
Michelle Ahazie <ahazie.michelle@gmail.com>
2022-05-30 16:36:04 +0100

    add arr in the main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const gameSquares = document.querySelectorAll(".jsgame__layout");
 
 let player = "x";
+let playerXArr = [];
+let playerOArr = [];
 
 const buttonClick = (event) => {
   if (player === "x") {
     event.target.innerHTML = "x";
+    playerXArr.push(event.target.id);
+    console.log(playerXArr);
     player = "o";
   } else {
     event.target.innerHTML = "o";
+    playerOArr.push(event.target.id)
     player = "x";
   }
 };
@@ -18,16 +23,24 @@ gameSquares.forEach((square) => {
 
 // getting the id from the HTML
 
-const goingOverId = () => {
-  for (let i = 0; i < gameSquares.length; i++) {
-    console.log(this.id);
-  }
-};
+let idArr = [];
+gameSquares.forEach((square) => {
+  let id = square.id;
+  idArr.push(id);
+});
+console.log(idArr);
 
 // create a variable for the winning combination for the game
-const winningScore = [(1, 5, 9)][(3, 5, 7)][(1, 2, 3)][(4, 5, 6)][(7, 8, 9)][
-  (1, 4, 7)
-][(2, 5, 8)][(3, 6, 9)];
+const winningScore = [
+  [1, 5, 9],
+  [3, 5, 7],
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+];
 
 // create a function to see if the player has won the game.
 
@@ -39,4 +52,4 @@ const winningPlayer = (player) => {
   }
 };
 
-gameSquares.addEventListener("click", winningPlayer);
+// gameSquares.addEventListener("click", winningPlayer);
